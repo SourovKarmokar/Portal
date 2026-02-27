47e0d5997433e96f1888b6f2049f1fa3c73072cb
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-02-28 03:16:27 +0600

    Portal

--- src/Portal.jsx
+++ src/Portal.jsx
@@ -1,9 +1,15 @@
 import { createPortal } from "react-dom";
+import { useEffect, useState } from "react";
 
 const Portal = ({children}) => {
     const mountElement = document.getElementById('portal-root')
+    const [elementDiv] = useState(() => document.createElement('div'))
 
-    return createPortal(children, mountElement)
+    useEffect(() => {
+        mountElement.appendChild(elementDiv)
+    } , [elementDiv, mountElement]);
+
+    return createPortal(children, elementDiv)
 }
 
 export default Portal ;
--- src/modal/ModelContent.jsx
+++ src/modal/ModelContent.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+const Child = () => {
+  return(
+    <div className='border border-red-500 p-1 text-white'>
+      <h1>I am a child</h1>
+      <button>Click</button>
+    </div>
+  )
+}
 
 const ModelContent = ({onClose}) => {
   return (
-    <div className='flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white p-2 '>
+    <div className='flex flex-col items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white  p-2 ' role="dialog" aria-modal='true'>
       <p className='mx-2'>Hey I am a Modal</p>
+      <Child/>
       <button 
       className='bg-white text-black p-1 border rounded-md'
       onClick={onClose}>Close</button>
